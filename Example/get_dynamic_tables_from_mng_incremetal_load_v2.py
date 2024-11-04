5d0575ebc08255554e930413b7b13e3cfc812eaf
haimron91@gmail.com <haimron91@gmail.com>
2024-11-04 14:21:20 +0000

    test 1

---
+++ Example/get_dynamic_tables_from_mng_incremetal_load_v2.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### This notebook as getting all tables from mng schema to load from pc-rep to bronze and save as parameter.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### This is a test !!
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# Create the list of dictionaries
+df = spark.table("pcil_travel.mng.tables_to_load")
+
+# Filter the DataFrame where 'load_type' equals 'full'
+filtered_df = df.filter(col("load_type") == "incremental")
+
+# Get only table name
+filtered_df = filtered_df.select('table_name', 'load_by_1', 'load_by_2')
+
+# Show the filtered DataFrame
+display(filtered_df)
+
+# Convert the filtered DataFrame back to a list of dictionaries
+result = filtered_df.collect()
+table_list = [row.asDict() for row in result]
+
+# Show the resulting list
+print(table_list)
+
+dbutils.jobs.taskValues.set(
+"table_list", table_list
+)
